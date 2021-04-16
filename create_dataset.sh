e4e877c0b634bb555a41dbfde638f0330107b1b1
tguardi <t.guardi@gmail.com>
2021-04-16 16:59:59 -0500

    Add files via upload

--- create_dataset.sh
+++ create_dataset.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-clearml-data create --project task1 --name clean1
-# clearml-data sync --folder ./data/
-clearml-data sync --folder /MSCA/MLOps/ClearML/data
+clearml-data create --project assignment1 --name data_dirty
+# sync to local folder:
+# clearml-data sync --folder {insert path to local folder with your data}
+clearml-data sync --folder /Users/guardi/MSCA/MLOps/ClearML/Assignment1/data
